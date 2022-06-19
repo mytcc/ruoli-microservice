8f3cef20515230200e76c0c9bdebfdddf195c670
ruoli <mytcc@vip.qq.com>
2022-06-19 21:57:37 +0800

    初始化项目

---
+++ common/src/main/java/ruoli/work/vo/CommonResponse.java
@@ -0,0 +1,34 @@
+package ruoli.work.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * 通用响应对象
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponse<T> implements Serializable {
+    /**
+     * 返回编码
+     */
+    private Integer code;
+    /**
+     * 返回消息
+     */
+    private String message;
+    /**
+     * 返回业务参数
+     */
+    private T data;
+    public CommonResponse(Integer code,String message){
+        this.code=code;
+        this.message=message;
+    }
+}
