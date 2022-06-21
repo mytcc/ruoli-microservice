620d3dd981d42f006651e7fd506074725459d30f
ruoli <mytcc@vip.qq.com>
2022-06-21 22:35:46 +0800

    增加权限Token相关代码

---
+++ authority-center/src/test/java/service/EcommerceUserTest.java
@@ -0,0 +1,35 @@
+package service;
+
+import cn.hutool.crypto.digest.MD5;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import ruoli.work.dao.EcommerceUserDao;
+import ruoli.work.entity.EcommerceUser;
+
+/**
+ * <h1>EcommerceUser 相关的测试</h1>
+ * */
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class EcommerceUserTest {
+
+    @Autowired
+    private EcommerceUserDao ecommerceUserDao;
+
+    @Test
+    public void createUserRecord() {
+
+        EcommerceUser ecommerceUser = new EcommerceUser();
+        ecommerceUser.setUsername("ImoocQinyi@imooc.com");
+        ecommerceUser.setPassword(MD5.create().digestHex("12345678"));
+        ecommerceUser.setExtraInfo("{}");
+        log.info("save user: [{}]",
+                JSON.toJSON(ecommerceUserDao.save(ecommerceUser)));
+    }
+}
